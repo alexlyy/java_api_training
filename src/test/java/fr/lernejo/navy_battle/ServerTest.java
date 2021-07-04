cf9249291e9851effe12f56b337d7f19707c9396
alexlyy <alexandre.ly@epita.fr>
2021-07-05 00:19:17 +0200

    add test coverage

--- src/test/java/fr/lernejo/navy_battle/ServerTest.java
+++ src/test/java/fr/lernejo/navy_battle/ServerTest.java
@@ -1,5 +1,7 @@
 package fr.lernejo.navy_battle;
 
+import fr.lernejo.navy_battle.prototypes.ServerInfo;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
@@ -26,5 +28,22 @@ public class ServerTest {
             source +=inputLine;
         in.close();
         assertEquals("OK", source);
+    }
+    void testIt() {
+        var one = new ServerInfo("id", "url", "message");
+        assertEquals("id", one.getId());
+        assertEquals("url", one.getUrl());
+        assertEquals("message", one.getMessage());
+    }
+
+    @Test
+    void testJSON() {
+        JSONObject in = new JSONObject("{\"id\": \"my_id\", \"url\":\"my_url\",\"message\":\"my_message\"}");
+        var srv = ServerInfo.fromJSON(in);
+        assertEquals("my_id", srv.getId());
+        assertEquals("my_url", srv.getUrl());
+        assertEquals("my_message", srv.getMessage());
+
+        assertEquals(srv.toJSON().toString(), in.toString());
     }
 }
